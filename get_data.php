bfd6033ff640c668fc0e469a56a3d7437eae2889
nuriayunika <riayunika888@gmail.com>
2026-06-03 16:29:47 +0700

    a

--- get_data.php
+++ get_data.php
@@ -1,5 +1,11 @@
 <?php
-error_reporting(0); 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 include 'koneksi.php';
 
 // 1. TANGKAP PARAMETER
--- get_detail_karyawan.php
+++ get_detail_karyawan.php
@@ -1,6 +1,11 @@
 <?php
-header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 include 'koneksi.php';
 
